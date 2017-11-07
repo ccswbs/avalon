23a2455f7284536d61e99f81a23677909293e30f
M. Brent Harp <brharp@uoguelph.ca>
2017-11-07 14:54:01 -0500

    Add legacy database connection

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -16,6 +16,17 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  */
 include __DIR__ . "/settings.pantheon.php";
 
+$databases['legacy']['default'] = array(
+  'database' => 'pantheon',
+  'username' => 'pantheon',
+  'password' => 'e484a51f1e2e46c3814454a3297083e8',
+  'prefix' => '',
+  'host' => 'dbserver.dev.50639f51-3c21-4ae8-a4da-50c3ea433508.drush.in',
+  'port' => '19837',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
 /**
  * If there is a local settings file, then include it
  */
@@ -24,3 +35,4 @@ if (file_exists($local_settings)) {
   include $local_settings;
 }
 $settings['install_profile'] = 'standard';
+
