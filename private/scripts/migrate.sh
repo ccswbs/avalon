2f4597032cbd20bab4ffa094c9182ed66ffb2b02
M. Brent Harp <brharp@uoguelph.ca>
2017-11-20 16:33:17 -0500

    Add second call to migrate-import with --update flag

--- private/scripts/migrate.sh
+++ private/scripts/migrate.sh
@@ -8,4 +8,5 @@ terminus env:wake hjckrrh.dev
 sleep 10 # give site a chance to wake up
 terminus remote:drush ug-avalon.dev -- migrate-rollback --all </dev/null
 terminus remote:drush ug-avalon.dev -- migrate-import --all </dev/null
+terminus remote:drush ug-avalon.dev -- migrate-import --update --all </dev/null
 
