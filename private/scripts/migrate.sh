05c13b7da012ef2d49104e44eaf5a25f8bf8dd0b
M. Brent Harp <brharp@uoguelph.ca>
2017-11-22 15:38:36 -0500

    Allow environment variables to override target site.env in migrate.sh

--- private/scripts/migrate.sh
+++ private/scripts/migrate.sh
@@ -4,22 +4,26 @@
 # Run D7 -> D8 migration.
 #
 
+# Environment
+SITE=${SITE:-ug-avalon}
+ENV=${ENV:-dev}
+
 # Wake up source site
 terminus env:wake hjckrrh.dev </dev/null
 sleep 10 # give site a chance to wake up
 
 # Set target site to SFTP mode (required for install)
-terminus connection:set ug-avalon.dev sftp </dev/null
+terminus connection:set $SITE.$ENV sftp </dev/null
 
 # Re-install target site
-terminus remote:drush ug-avalon.dev -- site-install -y minimal --config-dir=sites/default/config </dev/null
+terminus remote:drush $SITE.$ENV -- site-install -y minimal --config-dir=sites/default/config </dev/null
 
 # Run migration
-terminus remote:drush ug-avalon.dev -- migrate-import --all </dev/null
+terminus remote:drush $SITE.$ENV -- migrate-import --all </dev/null
 
 # Update migration (second pass)
-terminus remote:drush ug-avalon.dev -- migrate-import --update --all </dev/null
+terminus remote:drush $SITE.$ENV -- migrate-import --update --all </dev/null
 
 # Flip target site back to git mode
-terminus connection:set ug-avalon.dev git </dev/null
+terminus connection:set $SITE.$ENV git </dev/null
 
