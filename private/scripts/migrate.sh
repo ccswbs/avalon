5aa77e6c766664574d4d3a2419691569fc9c7120
M. Brent Harp <brharp@uoguelph.ca>
2017-11-22 10:21:31 -0500

    Add reinstall to migration script

--- private/scripts/migrate.sh
+++ private/scripts/migrate.sh
@@ -4,9 +4,16 @@
 # Run D7 -> D8 migration.
 #
 
+# Wake up source site
 terminus env:wake hjckrrh.dev
 sleep 10 # give site a chance to wake up
-terminus remote:drush ug-avalon.dev -- migrate-rollback --all </dev/null
+
+# Re-install target site
+terminus remote:drush ug-avalon.dev -- site-install minimal --config-dir=sites/default/config
+
+# Run migration
 terminus remote:drush ug-avalon.dev -- migrate-import --all </dev/null
+
+# Update migration (second pass)
 terminus remote:drush ug-avalon.dev -- migrate-import --update --all </dev/null
 
