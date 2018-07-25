cdc7788b8fb3a46053223abc9244eb6a21dfaede
M. Brent Harp <brharp@uoguelph.ca>
2018-07-25 12:00:23 -0400

    Merge UG install profile.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -23,19 +23,21 @@ $config_directories = array(
   CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
 );
 
-$secrets_file = $_SERVER['HOME'] . '/files/private/secrets.json';
-if (file_exists($secrets_file)) {
-  $secrets = json_decode(file_get_contents($secrets_file), 1);
-  $databases['legacy']['default'] = array(
-    'database' => $secrets['legacy_database'],
-    'username' => $secrets['legacy_username'],
-    'password' => $secrets['legacy_password'],
-    'prefix' => '',
-    'host' => $secrets['legacy_host'],
-    'port' => $secrets['legacy_port'],
-    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-    'driver' => 'mysql',
-  );
+if (isset($SERVER['HOME'])) {
+  $secrets_file = $_SERVER['HOME'] . '/files/private/secrets.json';
+  if (file_exists($secrets_file)) {
+    $secrets = json_decode(file_get_contents($secrets_file), 1);
+    $databases['legacy']['default'] = array(
+      'database' => $secrets['legacy_database'],
+      'username' => $secrets['legacy_username'],
+      'password' => $secrets['legacy_password'],
+      'prefix' => '',
+      'host' => $secrets['legacy_host'],
+      'port' => $secrets['legacy_port'],
+      'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+      'driver' => 'mysql',
+    );
+  }  
 }
 
 /**
@@ -52,3 +54,10 @@ if (file_exists($local_settings)) {
  */
 $settings['install_profile'] = 'ug';
 
+$databases['default']['default'] = array (
+  'database' => 'sites/default/files/.ht.sqlite',
+  'prefix' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
+  'driver' => 'sqlite',
+);
+$settings['hash_salt'] = 'lT1d1zYjM7ZtHb5Hu7J9JZrPUzd7ltJrCvDtAOmvLwTqE9AkyJeYngYV6tcdjwBwABVOapO76A';
