7eee16fab5b9aa3f1b4a4be98cdaae96e7fc9258
M. Brent Harp <brharp@uoguelph.ca>
2018-07-27 11:26:00 -0400

    Add UG Landing Page module to UG install profile

--- web/profiles/ug/modules/ug/ug_landing_page/tests/src/Functional/LoadTest.php
+++ web/profiles/ug/modules/ug/ug_landing_page/tests/src/Functional/LoadTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\Tests\landing_page\Functional;
+namespace Drupal\Tests\ug_landing_page\Functional;
 
 use Drupal\Core\Url;
 use Drupal\Tests\BrowserTestBase;
@@ -8,7 +8,7 @@ use Drupal\Tests\BrowserTestBase;
 /**
  * Simple test to ensure that main page loads with module enabled.
  *
- * @group landing_page
+ * @group ug_landing_page
  */
 class LoadTest extends BrowserTestBase {
 
@@ -17,7 +17,7 @@ class LoadTest extends BrowserTestBase {
    *
    * @var array
    */
-  public static $modules = ['landing_page'];
+  public static $modules = ['ug_landing_page'];
 
   /**
    * A user with permission to administer site configuration.
